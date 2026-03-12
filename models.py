0cad185e93b3afb8c6d62ace7f2b15c64a9ac181
gelogonza <angelo3649@icloud.com>
2026-03-11 22:01:30 -0400

    added empty models.py with class list.

---
+++ models.py
@@ -0,0 +1,6 @@
+# ByteBites Data Models
+#
+# Customer  - Represents a registered user with a purchase history; supports user verification.
+# Item      - Represents a single menu item with a price, category, and popularity rating.
+# Menu      - Represents a collection of Items; supports filtering, adding, and removing items.
+# Order     - Represents a Customer's order containing selected Items; tracks total cost and date.
